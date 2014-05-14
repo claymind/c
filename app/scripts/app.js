8e02edcb40a9a47b20b46b725d0cfee505a5b7e9
Guillermo Misa <support@claymind.com>
2014-05-14 13:08:08 -0700

    nag active and disabled state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,18 +3,5 @@
 angular.module('claymindApp', [
   'ngCookies',
   'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
-  });
+  'ngSanitize'
+]);
